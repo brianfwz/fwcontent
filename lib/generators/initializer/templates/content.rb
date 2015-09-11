c63c413e313712a13b3b762116fd8b321e184fb6
Brian <brian@futureworkz.com>
2015-09-11 13:11:08 +0700

    update fwcontent gem

--- lib/generators/initializer/templates/content.rb
+++
@@ -1,9 +0,0 @@
-class Content < ActiveRecord::Base
-	validates :title, :description, presence: true
-
-	serialize :page
-
-	def page_enum
-    Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.sort
-   end
-end
