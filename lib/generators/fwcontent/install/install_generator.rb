9a9fa88c496423bacb8ed81a4ebe7978b632f494
Brian <brian@futureworkz.com>
2015-10-06 18:15:15 +0700

    update install_generator.rb

--- lib/generators/fwcontent/install/install_generator.rb
+++ lib/generators/fwcontent/install/install_generator.rb
@@ -21,10 +21,43 @@ module Fwcontent
       end
 
       def copy_initializer_file
-        @class_name = class_name
-        copy_file "content.rb", "app/models/#{file_name}.rb"
+        # copy_file "content.rb", "app/models/#{file_name}.rb"
         copy_file "_get_content_for.html.erb", "app/views/_get_content_for.html.erb"
-        copy_file "content_helper.rb", "app/helpers/#{file_name}_helper.rb"
+        # copy_file "content_helper.rb", "app/helpers/#{file_name}_helper.rb"
+      end
+
+      def create_helper_file
+        create_file "app/helpers/#{file_name}_helper.rb", <<-FILE
+module #{class_name}Helper
+  def get_content_for
+    #{class_name}.where("page like ?", "%#{params[:controller]}:#{params[:action]}\n%")
+  end
+end
+        FILE
+      end
+
+      def create_model_file
+        create_file "app/models/#{file_name}.rb", <<-FILE
+class #{class_name} < ActiveRecord::Base
+  extend Enumerize
+
+  validates :title, :description, presence: true
+
+  serialize :page
+
+  enumerize :position, in: [:banner, 
+                            :top_menu, 
+                            :bottom_menu, 
+                            :social, 
+                            :left, 
+                            :right, 
+                            :middle]
+
+  def page_enum
+    Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.sort
+  end
+end
+        FILE
       end
     end
   end
