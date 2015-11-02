b47234892624cd95d27f4653fb9493d38a1e5169
Brian <brian@futureworkz.com>
2015-11-02 17:38:25 +0700

    update content to active admin

--- lib/generators/fwcontent/admin/templates/activeadmin/content.rb
+++ lib/generators/fwcontent/admin/templates/activeadmin/content.rb
@@ -1,3 +1,13 @@
 ActiveAdmin.register <%= class_name %> do
 	permit_params :title, :description, :page, :position
+
+	form do |f|
+    f.inputs 'Create News' do
+      f.input :title
+      f.input :description
+      f.input :position, as: :select, collection: Position.all.map(&:title)
+      f.input :page
+    end
+    actions
+  end
 end
