00bafdacbb2ae9e6636e2cdc7c5643de9d00a4da
Brian <brian@futureworkz.com>
2015-11-02 14:17:24 +0700

    update content model

--- lib/generators/fwcontent/install/templates/content.rb
+++ lib/generators/fwcontent/install/templates/content.rb
@@ -5,13 +5,7 @@ class <%= class_name %> < ActiveRecord::Base
 
 	serialize :page
 
-	enumerize :position, in: [:banner, 
-														:top_menu, 
-														:bottom_menu, 
-														:social, 
-														:left, 
-														:right, 
-														:middle]
+	enumerize :position, in: Position.all.map(&:title)
 
 	def page_enum
     Rails.application.routes.routes.map(&:defaults).reject!(&:blank?).inject([]){|b, a| b << "#{a[:controller]}:#{a[:action]}" }.sort
