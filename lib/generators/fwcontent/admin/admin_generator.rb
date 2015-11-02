91652fa6b1c99f7af3d39691bc0df0a1d35d97bf
Brian <brian@futureworkz.com>
2015-11-02 17:16:00 +0700

    move active admin folder

--- lib/generators/fwcontent/admin/admin_generator.rb
+++ lib/generators/fwcontent/admin/admin_generator.rb
@@ -2,12 +2,13 @@ require 'rails/generators/migration'
 
 module Fwcontent
   module Generators
-    class ActiveadminGenerator < ::Rails::Generators::NamedBase
+    class AdminGenerator < ::Rails::Generators::NamedBase
       include Rails::Generators::Migration
       source_root File.expand_path('../templates', __FILE__)
       
 
       def copy_initializer_file
+        active = Bundler.load.specs.map { |spec| spec.name }
         if active.include? "activeadmin"
           template "activeadmin/content.rb", "app/admin/#{file_name}.rb"
           template "activeadmin/position.rb", "app/admin/position.rb"
